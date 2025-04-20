7a146b63ec2a33bd62851a5b6f10fad147435bb8
Alseny DIALLO <alsenydiallo120@gmail.com>
2025-04-20 07:02:28 +0000

    mm

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-echo "Installing requirements..."
-pip install -r requirements.txt
-echo "Running migrations..."
-python manage.py makemigrations --merge
+#!/bin/sh
+echo 'Running migrations...'
 python manage.py migrate
-echo "Collection statics"
-echo 'yes' | python manage.py collectstatic --noinput
-sleep 5
-# gunicorn core.wsgi:application -b 0:8001 -w "${GUNICORN_WORKERS}" --log-level DEBUG --reload --threads=10 --timeout=3600
-gunicorn core.wsgi:application -b 0:8001 -w "${GUNICORN_WORKERS}" --log-level DEBUG --threads=10 --timeout=3600
+echo 'Collecting static files...'
+python manage.py collectstatic --noinput --clear --no-post-process
